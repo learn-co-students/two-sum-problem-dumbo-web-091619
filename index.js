cd5eca4050119c5087d9b4babcee05eeebed29e4
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-01-31 19:44:06 -0500

    Done.

--- index.js
+++ index.js
@@ -16,40 +16,49 @@ function bruteForceTwoSum(array, sum){
    return nums
 }
 
-// let binarySearch = (array, x, start, end) => {
-//     if(start > end){
-//         return false
-//     }
 
-//     let mid = Math.floor((start + end) / 2)
+function hashTwoSum(array, sum){
+    let numsObj = {}
+    let sumNumsArray = []
+    for(let eachNum in array){
+        let num2 = sum - array[eachNum]
+        
+        if (num2 in numsObj){
+            sumNumsArray.push([num2, array[eachNum]])
+        }
+        numsObj[array[eachNum]] = eachNum
+    }
+    return sumNumsArray
+}
 
-//     if (array[mid] === x){
-//         return true
-//     }
-
-//     if (array[mid > x]){
-//         return binarySearch(array, x, start, mid-1)
-//     } else {
-//         return binarySearch(array, x, mid+1, end)
-//     }
-// }
+let binarySearch = (array, x, start=0, end=array.length-1) => {
+        let mid = Math.floor(start + (end - start)/2)
+        switch(true){
+        case array[mid] === x:
+            return array[mid]
+        case end - start === 0:
+            return false
+         case array[mid] > x:
+            return binarySearch(array, x, start, mid)
+         case array[mid] < x:
+            return binarySearch(array, x, mid+1, end)    
+    }
+}
 
 function binarySearchTwoSum(array, sum){
-    let sortedArray = array.sort()
     let nums = []
-    for(let x in sortedArray){
-        for(let y in sortedArray){
-            if (sortedArray[x] + sortedArray[y] === sum){
-                        if(!!nums.length){ 
-                            if (!nums.flat(Infinity).includes(sortedArray[x]) && !nums.flat(Infinity).includes(sortedArray[y])) {
-                            nums.push([sortedArray[x], sortedArray[y]])
-                        } 
-                    } else {
-                        nums.push([sortedArray[x], sortedArray[y]])
-                    }
-                }
+    let sortedArray = array.sort()
+    for (let i in array.length){
+        let siblingIndex = binarySearch(array, sum-array[i])
+        if (siblingIndex >= 0){
+            switch(true){
+                case i > 0 && array[i-1] === arra[i]:
+                    nums.push([array[i], array[siblingIndex]])
+                case i < array.length - 1 && array[i+1] === arr[i]:
+                    nums.push([array[i], array[siblingIndex]])
             }
-       }
+        }
+    }
    return nums
 }
 
@@ -62,18 +71,4 @@ function binaryMatch(array, sum){
             }
        }
    return false
-}
-
-function hashTwoSum(array, sum){
-    let numsObj = {}
-    let sumNumsArray = []
-    for(let eachNum in array){
-        let num2 = sum - array[eachNum]
-
-        if (num2 in numsObj){
-            sumNumsArray.push([num2, array[eachNum]])
-        }
-        numsObj[array[eachNum]] = eachNum
-       }
-   return sumNumsArray
 }
